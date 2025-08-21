bf11a4f8cadbb30fb9a0816a8502ece08810fb21
Tomas Ferrer <tomasf.ferrer@hotmail.com>
2025-08-20 23:33:05 -0300

    agrego proyecto

---
+++ app/layout.tsx
@@ -0,0 +1,55 @@
+import type React from "react"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+})
+
+export const metadata: Metadata = {
+  title: "Vexo Studio",
+  description: "Diseño de muebles de lujo con un toque moderno y elegante.",
+  generator: "Vexo Studio",
+  openGraph: {
+    siteName: "Vexo Studio",
+    title: "Vexo Studio",
+    description: "Vexo Studio",
+    type: "website",
+    images: [
+      {
+        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/opengraph-katachi.jpg-7vz2r3hxZA6woukGOmH115Fg7Piyjs.jpeg",
+        alt: "Vexo Studio",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_BE",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Vexo Studio",
+    description: "Vexo Studio",
+    images: [
+      {
+        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/opengraph-katachi.jpg-7vz2r3hxZA6woukGOmH115Fg7Piyjs.jpeg",
+        alt: "Vexo Studio",
+      },
+    ],
+    site: "Vexo Studio",
+  },
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" className={`${inter.variable} antialiased`}>
+      <body className="font-sans bg-neutral-50 text-neutral-900 overflow-x-hidden">{children}</body>
+    </html>
+  )
+}
